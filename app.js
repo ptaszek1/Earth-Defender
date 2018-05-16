235eab932419a9dc0147d27e570fba1f36a35f68
Mariusz Ptaszek <mariusz.ptaszek2@gmail.com>
2018-05-16 22:47:39 +0200

    Fixed bullet pos

--- app.js
+++ app.js
@@ -8,24 +8,28 @@ $(function () {
             });
         });
 
-        let $pos = 0;
 
         function fire() {
-            $(".galaxy").append($("<div>").addClass("bullet").css("top", 900));
-            $pos = $('.ship').offset().left
+            $(".galaxy").append($("<div>").addClass("bullet").css({
+                top: 900,
+                left: $('.ship').offset().left+50,
+            }));
         }
         $(document).click(fire);
 
+        let $pos = 0;
+
         function shipPos() {
-            console.log($('.ship').position());
+            console.log(arr1[count]);
+            return arr1[count]
         }
+
 
         function update() {
             $(".bullet").each(function() {
                 let top = $(this).offset().top;
                 $(this).css({
-                    top: top - 20,
-                    left: $('.ship').offset().left,
+                    top: top - 10,
             });
                 if($(this).offset().top < -10){
                     $(this).remove()
